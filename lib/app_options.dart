d828b873d580df9e96d65c1fa1d5766852d8a0cc
Alex Yevtushenko <a@qx.zone>
2023-02-28 00:22:56 +0200

    Something that could run locally

--- lib/app_options.dart
+++ lib/app_options.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names
 
 const bool USE_AUTH_EMULATOR = true;
 const bool USE_DATABASE_EMULATOR = true;
+const String GOOGLE_CLIENT_ID = '';
---
+++ lib/presentation/controls/profile.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class Profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Padding(padding: EdgeInsets.only(top: 150)),
+      Text('Profile settings'),
+    ]);
+  }
+}
