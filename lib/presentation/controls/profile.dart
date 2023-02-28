5f65da47bf7f4982420ce6151da9dc3c531d20c2
Alex Yevtushenko <a@qx.zone>
2023-02-28 13:44:06 +0200

    Implement Doorbell list

--- lib/presentation/controls/profile.dart
+++ lib/presentation/controls/profile.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/cupertino.dart';
 
 class Profile extends StatelessWidget {
+  Profile({
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Padding(padding: EdgeInsets.only(top: 150)),
-      Text('Profile settings'),
-    ]);
+    return SliverList(
+        delegate: SliverChildListDelegate.fixed(<Widget>[
+      Column(children: [
+        Padding(padding: EdgeInsets.only(top: 150)),
+        Text('Profile settings'),
+      ])
+    ]));
   }
 }
