68e80e0490268a41336bb8a118cd16287f6d067d
AY <a@qx.zone>
2023-03-04 00:10:29 +0200

    Finalize navigation&routing up to 97% readiness

--- lib/presentation/screens/main_screen.dart
+++ lib/presentation/screens/main_screen.dart
@@ -4,9 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:livekit_client/livekit_client.dart';
 import 'package:qrdoorbell_mobile/presentation/controls/event_list.dart';
 
+import '../../data.dart';
 import '../../routing/route_state.dart';
 import '../controls/doorbell_list.dart';
-import '../../model/doorbell.dart';
 import '../controls/profile.dart';
 
 class MainScreenTabPages {
@@ -25,14 +25,6 @@ class MainScreen extends StatelessWidget {
   final User user = FirebaseAuth.instance.currentUser!;
   late final CupertinoTabController _tabController;
   late final RouteState _routeState;
-
-  final List<DoorbellCardViewModel> doorbells = [
-    DoorbellCardViewModel(doorbell: Doorbell(id: '1', name: 'Doorbell 1'), announce: '2 missed calls'),
-    DoorbellCardViewModel(doorbell: Doorbell(id: '2', name: 'Doorbell 2')),
-    DoorbellCardViewModel(doorbell: Doorbell(id: '3', name: 'Doorbell My'), announce: 'Just created'),
-    DoorbellCardViewModel(doorbell: Doorbell(id: '4', name: 'Doorbell Yours')),
-    DoorbellCardViewModel(doorbell: Doorbell(id: '5', name: 'Doorbell Shared')),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -62,7 +54,13 @@ class MainScreen extends StatelessWidget {
         late final String title;
 
         if (index == MainScreenTabPages.doorbells) {
-          tabWidget = DoorbellList(doorbells: doorbells);
+          tabWidget = DoorbellList(
+            doorbells: DataStore()
+                .allDoorbells
+                .map((e) => DoorbellCardViewModel(doorbell: e, announce: e.id == '1' ? 'First one' : 'No new messages'))
+                .toList(),
+            onTapHandler: _onDoorbellSelected,
+          );
           title = 'Doorbells';
         } else if (index == MainScreenTabPages.events) {
           tabWidget = EventList();
@@ -94,17 +92,21 @@ class MainScreen extends StatelessWidget {
     );
   }
 
+  Future<void> _onDoorbellSelected(Doorbell doorbell) async {
+    await _routeState.go('/doorbells/${doorbell.id}');
+  }
+
   void _handleTabIndexChanged() {
     switch (_tabController.index) {
       case 1:
-        _routeState.go('/books/new');
+        _routeState.go('/events');
         break;
       case 2:
-        _routeState.go('/books/all');
+        _routeState.go('/profile');
         break;
       case 0:
       default:
-        _routeState.go('/books/popular');
+        _routeState.go('/doorbells');
         break;
     }
   }
