c89ce1637c434c10a9cadf4042e4fc468630b635
Alex Yevtushenko <a@qx.zone>
2023-04-26 01:20:30 +0300

    Fix video call control buttons Add session timer display

--- lib/presentation/controls/video/participant_widget.dart
+++ lib/presentation/controls/video/participant_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:collection/collection.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -57,13 +59,23 @@ class RemoteParticipantWidget extends ParticipantWidget {
 }
 
 abstract class _ParticipantWidgetState<T extends ParticipantWidget> extends State<T> {
+  Timer? timer;
   bool _visible = true;
   VideoTrack? get activeVideoTrack;
   TrackPublication? get videoPublication;
   TrackPublication? get firstAudioPublication;
+  Duration? callDuration;
 
   @override
   void initState() {
+    callDuration = Duration.zero;
+    timer = Timer(const Duration(seconds: 1), () {
+      if (callDuration != null)
+        setState(() {
+          callDuration = (callDuration ?? Duration.zero) + const Duration(seconds: 1);
+        });
+    });
+
     super.initState();
     widget.participant.addListener(_onParticipantChanged);
     _onParticipantChanged();
@@ -71,6 +83,7 @@ abstract class _ParticipantWidgetState<T extends ParticipantWidget> extends Stat
 
   @override
   void dispose() {
+    timer?.cancel();
     widget.participant.removeListener(_onParticipantChanged);
     super.dispose();
   }
@@ -86,107 +99,115 @@ abstract class _ParticipantWidgetState<T extends ParticipantWidget> extends Stat
   void _onParticipantChanged() => setState(() {});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
+  Widget build(BuildContext context) {
+    var room = context.findAncestorStateOfType<CallScreenState>()?.room;
+
+    return Scaffold(
         backgroundColor: CupertinoColors.darkBackgroundGray,
         body: SizedBox(
-          width: double.infinity,
-          height: double.infinity,
-          child: Stack(
-            fit: StackFit.expand,
-            alignment: Alignment.topCenter,
-            children: [
-              InkWell(
-                onTap: () => setState(() => _visible = !_visible),
-                child: activeVideoTrack != null
-                    ? VideoTrackRenderer(
-                        activeVideoTrack!,
-                        fit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
-                      )
-                    : const Text('NO VIDEO'),
-              ),
-              Column(
-                children: [
-                  const Padding(padding: EdgeInsets.only(top: 140)),
-                  Text(
-                    activeVideoTrack != null ? 'Active' : 'Connecting...',
-                    style: const TextStyle(color: Colors.white, fontSize: 32),
-                  ),
-                  const Padding(
-                      padding: EdgeInsets.only(top: 10),
-                      child: Text('00:02',
-                          style: TextStyle(
-                            color: CupertinoColors.white,
-                          ))),
-                  const Spacer(),
-                  Row(
-                    children: [
-                      const Spacer(),
-                      CupertinoButton(
-                          color: CupertinoColors.white.withOpacity(0.3),
-                          borderRadius: const BorderRadius.all(Radius.circular(55)),
-                          padding: const EdgeInsets.all(20),
-                          child: Icon(
-                            widget.participant.isMuted ? CupertinoIcons.mic_solid : CupertinoIcons.mic_slash_fill,
-                            color: CupertinoColors.white,
-                            size: 36,
-                          ),
-                          onPressed: () => setState(() {
-                                var room = context.findAncestorStateOfType<CallScreenState>()?.room;
-                                if (room != null && room.localParticipant != null)
-                                  room.localParticipant!.setMicrophoneEnabled(room.localParticipant!.isMuted);
-                              })),
-                      const Spacer(),
-                      CupertinoButton(
-                          color: CupertinoColors.white.withOpacity(0.3),
-                          borderRadius: const BorderRadius.all(Radius.circular(55)),
-                          padding: const EdgeInsets.all(20),
-                          child: Icon(
-                            widget.participant.hasVideo ? CupertinoIcons.video_camera_solid : CupertinoIcons.video_camera,
-                            color: CupertinoColors.white,
-                            size: 36,
-                          ),
-                          onPressed: () => setState(() {
-                                (widget.participant as LocalParticipant).setCameraEnabled(!widget.participant.hasVideo);
-                              })),
-                      const Spacer(),
-                      CupertinoButton(
-                          color: CupertinoColors.white.withOpacity(0.6),
-                          borderRadius: const BorderRadius.all(Radius.circular(55)),
-                          padding: const EdgeInsets.all(20),
-                          child: const Icon(
-                            CupertinoIcons.speaker_3_fill,
-                            color: CupertinoColors.white,
-                            size: 36,
-                          ),
-                          onPressed: () => setState(() {
-                                var room = context.findAncestorStateOfType<CallScreenState>()?.room;
-                                if (room != null) room.setSpeakerOn(!(room.speakerOn ?? false));
-                              })),
-                      const Spacer(),
-                    ],
-                  ),
-                  const Padding(padding: EdgeInsets.only(top: 60)),
-                  CupertinoButton(
-                      color: CupertinoColors.destructiveRed,
-                      borderRadius: const BorderRadius.all(Radius.circular(55)),
-                      padding: const EdgeInsets.all(20),
-                      child: const Icon(
-                        CupertinoIcons.phone_down_fill,
-                        color: CupertinoColors.white,
-                        size: 36,
-                      ),
-                      onPressed: () {
-                        widget.participant
-                            .unpublishAllTracks()
-                            .then((value) => widget.participant.dispose().then((value) => RouteStateScope.of(context).go('/doorbells')));
-                      }),
-                  const Padding(padding: EdgeInsets.only(top: 100)),
-                ],
-              ),
-            ],
-          ),
-        ),
-      );
+            width: double.infinity,
+            height: double.infinity,
+            child: Stack(
+              fit: StackFit.expand,
+              alignment: Alignment.topCenter,
+              children: [
+                InkWell(
+                  onTap: () => setState(() => _visible = !_visible),
+                  child: activeVideoTrack != null
+                      ? VideoTrackRenderer(
+                          activeVideoTrack!,
+                          fit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
+                        )
+                      : const Text('NO VIDEO'),
+                ),
+                Column(
+                  children: [
+                    const Padding(padding: EdgeInsets.only(top: 140)),
+                    Text(
+                      activeVideoTrack != null ? 'Active' : 'Connecting...',
+                      style: const TextStyle(color: Colors.white, fontSize: 32),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: callDuration != null
+                            ? Text(_printDuration(callDuration!),
+                                style: const TextStyle(
+                                  color: CupertinoColors.white,
+                                ))
+                            : Container()),
+                    const Spacer(),
+                    Row(
+                      children: [
+                        const Spacer(),
+                        CupertinoButton(
+                            color: CupertinoColors.white.withOpacity(0.3),
+                            borderRadius: const BorderRadius.all(Radius.circular(55)),
+                            padding: const EdgeInsets.all(20),
+                            child: Icon(
+                              room?.localParticipant?.isMuted == false ? CupertinoIcons.mic_solid : CupertinoIcons.mic_slash_fill,
+                              color: CupertinoColors.white,
+                              size: 36,
+                            ),
+                            onPressed: () => setState(() {
+                                  room?.localParticipant?.setMicrophoneEnabled(!(room.localParticipant?.isMuted ?? false));
+                                })),
+                        const Spacer(),
+                        CupertinoButton(
+                            color: CupertinoColors.white.withOpacity(0.3),
+                            borderRadius: const BorderRadius.all(Radius.circular(55)),
+                            padding: const EdgeInsets.all(20),
+                            child: Icon(
+                              room?.localParticipant?.hasVideo == true ? CupertinoIcons.video_camera_solid : CupertinoIcons.video_camera,
+                              color: CupertinoColors.white,
+                              size: 36,
+                            ),
+                            onPressed: () => setState(() {
+                                  room?.localParticipant?.setCameraEnabled(!(room.localParticipant?.hasVideo ?? false));
+                                })),
+                        const Spacer(),
+                        CupertinoButton(
+                            color: CupertinoColors.white.withOpacity(0.6),
+                            borderRadius: const BorderRadius.all(Radius.circular(55)),
+                            padding: const EdgeInsets.all(20),
+                            child: const Icon(
+                              CupertinoIcons.speaker_3_fill,
+                              color: CupertinoColors.white,
+                              size: 36,
+                            ),
+                            onPressed: () => setState(() {
+                                  room?.setSpeakerOn(!(room.speakerOn ?? false));
+                                })),
+                        const Spacer(),
+                      ],
+                    ),
+                    const Padding(padding: EdgeInsets.only(top: 60)),
+                    CupertinoButton(
+                        color: CupertinoColors.destructiveRed,
+                        borderRadius: const BorderRadius.all(Radius.circular(55)),
+                        padding: const EdgeInsets.all(20),
+                        child: const Icon(
+                          CupertinoIcons.phone_down_fill,
+                          color: CupertinoColors.white,
+                          size: 36,
+                        ),
+                        onPressed: () {
+                          widget.participant
+                              .unpublishAllTracks()
+                              .then((value) => widget.participant.dispose().then((value) => RouteStateScope.of(context).go('/doorbells')));
+                        }),
+                    const Padding(padding: EdgeInsets.only(top: 100)),
+                  ],
+                ),
+              ],
+            )));
+  }
+
+  String _printDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return "${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds";
+  }
 }
 
 class _LocalParticipantWidgetState extends _ParticipantWidgetState<LocalParticipantWidget> {
