7b1e104702a5f977d7d450cb7aa8c550cc9e2ca4
AY <a@qx.zone>
2023-06-13 12:11:44 +0300

    Fixing data handling approach

--- lib/presentation/controls/doorbell_list.dart
+++ lib/presentation/controls/doorbell_list.dart
@@ -1,5 +1,5 @@
-import 'package:collection/collection.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 import '../../data.dart';
 import 'doorbell_card.dart';
 
@@ -18,18 +18,23 @@ class DoorbellList extends StatefulWidget {
 class DoorbellListState extends State<DoorbellList> {
   @override
   Widget build(BuildContext context) {
-    final dataStore = DataStore.of(context);
-    var data = dataStore.doorbells.items.sortedByCompare((x) => x, (a, b) {
-      if (a.lastEvent != null && b.lastEvent != null) return b.lastEvent!.dateTime.isAfter(a.lastEvent!.dateTime) ? 1 : -1;
-      return a.name.compareTo(b.name);
-    });
-    return SliverList.list(
-        children: data
-            .map((x) => DoorbellCard(
-                  doorbell: x,
-                  announce: x.lastEvent != null ? "${x.lastEvent!.formattedName} ${x.lastEvent!.formattedDateTimeSingleLine}" : 'No events',
-                  onTapHandler: widget.onTapHandler,
-                ))
-            .toList());
+    // final dataStore = DataStore.of(context);
+    // var data = dataStore.doorbells.items.sortedByCompare((x) => x, (a, b) {
+    //   if (a.lastEvent != null && b.lastEvent != null) return b.lastEvent!.dateTime.isAfter(a.lastEvent!.dateTime) ? 1 : -1;
+    //   return a.name.compareTo(b.name);
+    // });
+    return ChangeNotifierProvider(
+        create: (context) => DataStore.of(context).doorbells,
+        builder: (context, child) => Consumer<DataStoreRepository<Doorbell>>(
+            builder: (context, doorbells, child) => SliverList.list(
+                children: doorbells.items
+                    .map((x) => DoorbellCard(
+                          doorbell: x,
+                          announce: x.lastEvent != null
+                              ? "${x.lastEvent!.formattedName} ${x.lastEvent!.formattedDateTimeSingleLine}"
+                              : 'No events',
+                          onTapHandler: widget.onTapHandler,
+                        ))
+                    .toList())));
   }
 }
